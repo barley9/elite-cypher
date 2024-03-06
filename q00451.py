5dbd4e5b37714706b7a46f55642a5e121d4eb1b8
barley9 <barlimanbutterbur9@gmail.com>
2024-03-06 16:25:31 -0500

    added solution to problem 142. Linked List Cycle II

--- q00451.py
+++ q00451.py
@@ -1,7 +1,9 @@
 """
 451. Sort Characters By Frequency
 
-Given a string `s`, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+Given a string `s`, sort it in decreasing order based on the frequency of the
+characters. The frequency of a character is the number of times it appears in
+the string.
 
 Return the sorted string. If there are multiple answers, return any of them.
 """
@@ -25,9 +27,11 @@ class Solution:
         
     def frequencySort(self, s: str) -> str:
         """
-        One-liner solution using `collections.Counter`. Because of sorting, O(n log n).
+        One-liner solution using `collections.Counter`. Because of sorting,
+        O(n log n).
         """
-        # Get `Counter()` of character frequencies, sort descending by frequency, then join frequency-length runs of each character
+        # Get `Counter()` of character frequencies, sort descending by
+        # frequency, then join frequency-length runs of each character
         return ''.join(item[0] * item[1] for item in sorted(
             collections.Counter(s).items(),
             key=lambda item: item[1],
