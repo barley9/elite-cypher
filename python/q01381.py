20876e76d5811e883383077e1b774383a5ddcad1
barley9 <barlimanbutterbur9@gmail.com>
2024-09-30 11:12:18 -0400

    added solution to problem 1381. Design a Stack With Increment Operation

---
+++ python/q01381.py
@@ -0,0 +1,42 @@
+"""
+1381. Design a Stack With Increment Operation
+
+Design a stack that supports increment operations on its elements.
+
+Implement the `CustomStack` class:
+    `CustomStack(int maxSize)` Initializes the object with maxSize which is the
+        maximum number of elements in the stack.
+    `void push(int x)` Adds `x` to the top of the stack if the stack has not
+        reached the `maxSize`.
+    `int pop()` Pops and returns the top of the stack or `-1` if the stack is
+        empty.
+    `void inc(int k, int val)` Increments the bottom `k` elements of the stack
+        by `val`. If there are less than `k` elements in the stack, increment
+        all the elements in the stack.
+"""
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
+
+class CustomStack:
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.max_size = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if self.stack:
+            return self.stack.pop()
+        else:
+            return -1        
+
+    def increment(self, k: int, val: int) -> None:
+        """O(maxSize) time, O(1) space"""
+        for i in range(min(len(self.stack), k)):
+            self.stack[i] += val
