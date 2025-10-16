a209804bb06d79a39c62000c25fe99abe82a339d
barley9 <barlimanbutterbur9@gmail.com>
2025-10-16 17:03:46 -0400

    added python solution to problem 2598. Smallest Missing Non-negative Integer After Operations

---
+++ python/q02598.py
@@ -0,0 +1,34 @@
+"""
+2598. Smallest Missing Non-negative Integer After Operations
+
+You are given a 0-indexed integer array `nums` and an integer `value`.
+
+In one operation, you can add or subtract `value` from any element of `nums`.
+    For example, if `nums = [1,2,3]` and `value = 2`, you can choose to
+        subtract `value` from `nums[0]` to make `nums = [-1,2,3]`.
+
+The MEX (minimum excluded) of an array is the smallest missing non-negative
+integer in it.
+    For example, the MEX of `[-1,2,3]` is `0` while the MEX of `[1,0,3]` is `2`.
+
+Return the maximum MEX of `nums` after applying the mentioned operation any
+number of times.
+"""
+
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        """O(n) time, O(n) space solution"""
+        residues = {}
+        for n in nums:
+            r = n % value
+            if r in residues:
+                residues[r] += 1
+            else:
+                residues[r] = 1
+
+        for i in range(10 ** 5 + 1):
+            r = i % value
+            if (r in residues) and (residues[r] > 0):
+                residues[r] -= 1
+            else:
+                return i
