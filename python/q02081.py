9ec695f23cf3e6a8c2ef96bf95f17dc976e16ed6
barley9 <barlimanbutterbur9@gmail.com>
2025-10-29 10:40:14 -0400

    made minor comment formatting changes

--- python/q02081.py
+++ python/q02081.py
@@ -3,7 +3,7 @@
 
 A k-mirror number is a positive integer without leading zeros that reads the
 same both forward and backward in base-10 as well as in base-k.
-    For example, `9` is a 2-mirror number. The representation of 9 in base-10
+    For example, `9` is a 2-mirror number. The representation of `9` in base-10
         and base-2 are `9` and `1001` respectively, which read the same both
         forward and backward.
     On the contrary, `4` is not a 2-mirror number. The representation of `4`
