57c18ef735e9943136703b9982c917e0474f9e83
barley9 <barlimanbutterbur9@gmail.com>
2025-11-18 17:07:06 -0500

    added python solution to problem 717. 1-bit and 2-bit Characters

---
+++ python/q00717.py
@@ -0,0 +1,42 @@
+"""
+717. 1-bit and 2-bit Characters
+
+We have two special characters:
+    The first character can be represented by one bit `0`.
+    The second character can be represented by two bits (`10` or `11`).
+
+Given a binary array `bits` that ends with `0`, return `true` if the last
+character must be a one-bit character.
+"""
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        """
+        O(n) time, O(n) space solution
+
+        Strategy: Represent as two-state FSM
+        """
+        # States
+        ZERO = 0
+        ONE  = 1
+
+        state = ZERO  # start in ZERO state
+        decoded = []  # decoded characters
+        for b in bits:
+            if state == ZERO:
+                if b == 0:
+                    state = ZERO
+                    decoded.append('a')
+                else:
+                    state = ONE
+            elif state == ONE:
+                if b == 0:
+                    state = ZERO
+                    decoded.append('b')
+                else:
+                    state = ZERO
+                    decoded.append('c')
+
+        # print(decoded)
+        
+        return decoded[-1] == 'a'
